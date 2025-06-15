fdbbcd1d7ced964da589fd8698425d2e976b773a
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-06-15 21:12:40 +0530

    Create V1.0.3__create_table1.sql

---
+++ dbscripts/V1.0.3__create_table1.sql
@@ -0,0 +1,7 @@
+--! Description: Create users table
+CREATE OR REPLACE TABLE my_schema1.cust (
+    id            INT AUTOINCREMENT PRIMARY KEY,
+    name          STRING NOT NULL,
+    email         STRING UNIQUE,
+    created_at    TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP
+);
