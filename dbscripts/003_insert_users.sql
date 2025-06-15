3b61890836a76c4e076988e50fd46ee37d351a22
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-06-15 17:38:10 +0530

    Update 003_insert_users.sql

--- dbscripts/003_insert_users.sql
+++ dbscripts/003_insert_users.sql
@@ -1,5 +1,8 @@
 --! Description: Insert sample users
-INSERT INTO my_schema.users (name, email) VALUES 
+USE DATABASE SAMPLE;
+USE SCHEMA my_schema;
+
+INSERT INTO users (name, email) VALUES 
   ('Alice', 'alice@example.com'),
   ('Bob', 'bob@example.com'),
   ('Charlie', 'charlie@example.com');
