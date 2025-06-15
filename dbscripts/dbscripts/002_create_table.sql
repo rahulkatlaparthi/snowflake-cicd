d822d01ead89696c6c3332d66d2f01501c656540
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-06-15 17:43:55 +0530

    Delete dbscripts/dbscripts/002_create_table.sql

--- dbscripts/dbscripts/002_create_table.sql
+++
@@ -1,7 +0,0 @@
---! Description: Create users table
-CREATE OR REPLACE TABLE my_schema.customer (
-    id            INT AUTOINCREMENT PRIMARY KEY,
-    name          STRING NOT NULL,
-    email         STRING UNIQUE,
-    created_at    TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP
-);
