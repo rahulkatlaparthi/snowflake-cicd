b3d91a204f2ed492847b89dab65c6895af7ca184
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-06-15 18:15:29 +0530

    Update and rename 003_insert_users.sql to V1.0.2__insert_users_table.sql

--- dbscripts/V1.0.2__insert_users_table.sql
+++ dbscripts/V1.0.2__insert_users_table.sql
@@ -1,6 +1,6 @@
 --! Description: Insert sample users
 USE DATABASE SAMPLE;
-USE SCHEMA my_schema;
+USE SCHEMA my_schema1;
 
 INSERT INTO users (name, email) VALUES 
   ('Alice', 'alice@example.com'),
