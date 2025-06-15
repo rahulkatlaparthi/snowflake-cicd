d5a24ccba2e59c555ae830ded864a73013656b13
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-06-15 21:20:04 +0530

    Create V1.0.4__insertsql.sql

---
+++ dbscripts/V1.0.4__insertsql.sql
@@ -0,0 +1,6 @@
+-- Insert sample data into the table
+INSERT INTO my_schema1.cust (name, email)
+VALUES 
+  ('Alice', 'alice@example.com'),
+  ('Bob', 'bob@example.com'),
+  ('Charlie', 'charlie@example.com');
