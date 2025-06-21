f985672cfb2a894c309505ca90ef607b795b7675
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-06-21 20:43:00 +0530

    Create V1.0.6__altersql.sql

---
+++ dbscripts/V1.0.6__altersql.sql
@@ -0,0 +1,6 @@
+-- Insert sample data into the table
+INSERT INTO my_schema.cust (name, email)
+VALUES 
+  ('Alicea', 'alicea@example.com'),
+  ('Boba', 'boba@example.com'),
+  ('Charliea', 'charliea@example.com');
