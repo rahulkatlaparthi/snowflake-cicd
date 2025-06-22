0adc2d0c8b216be16931f0eb96a2bf0b5c6effcf
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-06-22 11:06:23 +0530

    Update stg_orders.sql

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,12 +1,7 @@
-with source as (
-    select * from {{ source('raw', 'orders') }}
-),
-renamed as (
-    select
-        order_id,
-        customer_id,
-        cast(order_date as date) as order_date,
-        total_amount
-    from source
-)
-select * from renamed
+-- models/staging/stg_orders.sql
+
+select
+  order_id,
+  customer_id,
+  order_date
+from {{ source('raw', 'orders') }}
